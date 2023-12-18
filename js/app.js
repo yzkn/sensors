70c4b5085d27b95927c91267b9016a973a826628
YA <9039417+yzkn@users.noreply.github.com>
2023-12-18 22:45:54 +0900

    init

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
+
+
+window.addEventListener('DOMContentLoaded', _ => {
+
+    document.getElementById('load').addEventListener('click', event => {
+        console.log(event);
+    });
+
+});
