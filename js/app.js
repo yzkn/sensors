3cfbea11d6e7b64a175e1f4f749abf211828362d
YA <9039417+yzkn@users.noreply.github.com>
2023-12-18 23:40:29 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,74 @@
 // Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
 
 
-window.addEventListener('DOMContentLoaded', _ => {
+let map;
+const initMap = () => {
+    map = L.map('map');
 
-    document.getElementById('load').addEventListener('click', event => {
-        console.log(event);
-    });
+    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+        attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+    }).addTo(map);
+
+    map.setView([35.681236, 139.767125], 10);
+};
+
+
+const updateLocationResult = (coords) => {
+    document.getElementById('locationResult').value = `${coords.latitude},${coords.longitude} (${coords.accuracy}m)` + '\n' +
+        (coords.altitude ? `${coords.altitude} (${coords.altitudeAccuracy}m)` : '') + '\n' +
+        (coords.heading ? `${coords.heading}deg` : '') + '\n' +
+        (coords.speed ? `${coords.speed} m / s` : '');
+};
+
+
+window.addEventListener('DOMContentLoaded', _ => {
+    initMap();
+
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition((pos) => {
+            const c = pos.coords;
+            updateLocationResult(c);
+            if (map) {
+                map.setView([c.latitude, c.longitude], 16);
+            }
+        }, (error) => {
+            console.error(error);
+        }, {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        });
+    }
+
+    if (DeviceMotionEvent.requestPermission) {
+        DeviceMotionEvent.requestPermission()
+            .then(permissionState => {
+                if (permissionState === 'granted') {
+                    window.addEventListener("devicemotion", function (event) {
+                        if (event.accelerationIncludingGravity) {
+                            document.getElementById('motionResult').value = event.acceleration.x + ',' + event.acceleration.y + ',' + event.acceleration.z + '\n' +
+                                event.accelerationIncludingGravity.x + ',' + event.accelerationIncludingGravity.y + ',' + event.accelerationIncludingGravity.z + '\n' +
+                                event.rotationRate.alpha + ',' + event.rotationRate.beta + ',' + event.rotationRate.gamma
+                                ;
+                        }
+                    });
+                }
+            })
+            .catch(console.error);
+    }
+
+    if (window.DeviceOrientationEvent) {
+        window.addEventListener(
+            "deviceorientation",
+            (event) => {
+                // alpha: rotation around z-axis var rotateDegrees = event.alpha;
+                // gamma: left to right          var leftToRight = event.gamma;
+                // beta: front back motion       var frontToBack = event.beta;
+
+                document.getElementById('orientationResult').value = event.alpha + ',' + event.gamma + ',' + event.beta;
+            },
+            true,
+        );
+    }
 
 });
